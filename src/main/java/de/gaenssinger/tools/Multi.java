f84d4f6f3185290c59e16cd3d85fc9194c71fb36
macGYves <macgyves@gaenssinger.de>
2021-10-12 20:54:22 +0200

    Init repo

---
+++ src/main/java/de/gaenssinger/tools/Multi.java
@@ -0,0 +1,10 @@
+package de.gaenssinger.tools;
+
+public class Multi {
+
+  public static void main(String... args){
+    System.out.println("I did it!");
+    System.out.println("I did it!");
+
+  }
+}
